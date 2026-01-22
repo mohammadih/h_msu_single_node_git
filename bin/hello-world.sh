1916b782ddc34c74acb828030520146b0039fbe1
Kirk Webb <kwebb@cs.utah.edu>
2026-01-22 15:45:31 -0700

    Updates to "hello world" script.

--- bin/hello-world.sh
+++ bin/hello-world.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-MYFILE=/var/tmp/$1
+MYFILE="/var/tmp/$1"
 
 if [ ! -f $MYFILE ]; then
     touch $MYFILE && \
 	echo "Hello World!" > $MYFILE || \
-	    { echo "Failed to write to $MYFILE"; exit 1 }
+	    { echo "Failed to write to $MYFILE"; exit 1; }
 fi
 
 exit 0
