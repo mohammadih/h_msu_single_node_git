9f71a82960c001b41afba28509774a21c8cf544c
Kirk Webb <kwebb@cs.utah.edu>
2026-01-22 22:46:10 -0700

    Updates to hands-on instructions in the comments.

--- profile.py
+++ profile.py
@@ -20,35 +20,45 @@ request = pc.makeRequestRSpec()
 
 # Parameter specifying the directory path for the startup script's output file.
 pc.defineParameter(
-    "directory_path",
-    "Hello world directory path",
-    portal.ParameterType.STRING, DEFAULT_DIRECTORY_PATH,
-    longDescription="Filesystem directory path where the output of the 'hello-world' startup script will be created.")
+    name = "directory_path",
+    description = "Hello world directory path",
+    typ = portal.ParameterType.STRING,
+    defaultValue = DEFAULT_DIRECTORY_PATH,
+    longDescription = "Filesystem directory path where the output of the 'hello-world' startup script will be created.")
 
 #
-# Add a filename parameter here for where the output of the startup script
-# will go. Call the parameter (first argument to `defineParameter`) 'file_name'.
+# *** FIRST REQUIRED CHANGE: Add a filename parameter here for where
+# the output of the startup script will go. The `name` argument should
+# be set to `file_name`. Use the `defineParameter()` call above as a
+# template. Do not simply uncomment the line below. That will not work...
 #
 # portal.context.defineParameter(...)
 
-# Process the defined parameters
+# Process the parameters defined above
 params = pc.bindParameters()
 
+# Request a single "raw" (bare metal) PC (compute) node, and set some
+# attributes based on globals defined above.
 node = request.RawPC("n1")
 if 'IMAGE_URN' in globals() and IMAGE_URN:
     node.disk_image = IMAGE_URN
 if 'HARDWARE_TYPE' in globals() and HARDWARE_TYPE:
     node.hardware_type = HARDWARE_TYPE
+# See below for the second change that needs to be made to this script.
 if hasattr(params, "directory_path") and hasattr(params, "file_name"):
     #
-    # Build up a 'startup_script' string variable here consisting of
-    # the SCRIPT_PATH global variable, followed by a space, followed
-    # by the 'directory_path' parameter, followed by a forward slash
-    # ('/'), then followed lastly by the 'file_name' parameter.
-    # Note that parameters are stored as attributes of the parameters
-    # object. E.g., 'params.directory_path'
+    # *** SECOND REQUIRED CHANGE: Build up a 'startup_script' string
+    # variable, consisting first of the SCRIPT_PATH global variable,
+    # followed by a space, then the 'directory_path' profile
+    # parameter, next a forward slash ('/'), then followed lastly by
+    # the 'file_name' profile parameter.  Note that profile parameters
+    # are stored as attributes of the `params` object returned by the
+    # `pc.bindParameters()` call above. E.g., 'params.directory_path'.
     #
-    # startup_script = ...
+    # Note: Don't simply uncomment the line below; doing so will
+    # result in a syntax error!
+    #
+    # startup_script = "{} ...".format(SCRIPT_PATH, ..)
     node.addService(pg.Execute(shell="bash", command=startup_script))
 
 # The portal expects this function to be called a the end of a profile script.
